6e1dde40fb63834ba5203f524ea348cabbeccb94
unknown <teixeira308@hotmail.com>
2021-03-24 02:09:17 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/v1/games', (req, res) => {
+    res.send(req);
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
